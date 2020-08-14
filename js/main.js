ba10879fc5fb8525e796e5034b17ae21529ac7c9
liven <ulucqs@gmail.com>
2020-08-14 02:27:38 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+function randomColor() {
+    var rdm = Math.floor(Math.random() * Math.floor(14));
+    var UIColor;
+    var UILogo;
+    switch (rdm) {
+        case 1:
+            UIColor = "#1abc9c";
+            UILogo = "logo-1.jpg";
+            break;
+        case 2:
+            UIColor = "#2ecc71";
+            UILogo = "logo-2.jpg";
+            break;
+        case 3:
+            UIColor = "#3498db";
+            UILogo = "default.jpg";
+            break;
+        case 4:
+            UIColor = "#9b59b6";
+            UILogo = "logo-4.jpg";
+            break;
+        case 5:
+            UIColor = "#16a085";
+            UILogo = "logo-5.jpg";
+            break;
+        case 6:
+            UIColor = "#27ae60";
+            UILogo = "logo-6.jpg";
+            break;
+        case 7:
+            UIColor = "#2980b9";
+            UILogo = "logo-7.jpg";
+            break;
+        case 8:
+            UIColor = "#8e44ad";
+            UILogo = "logo-8.jpg";
+            break;
+        case 9:
+            UIColor = "#f1c40f";
+            UILogo = "logo-9.jpg";
+            break;
+        case 10:
+            UIColor = "#e67e22";
+            UILogo = "logo-10.jpg";
+            break;
+        case 11:
+            UIColor = "#e74c3c";
+            UILogo = "logo-11.jpg";
+            break;
+        case 12:
+            UIColor = "#f39c12";
+            UILogo = "logo-12.jpg";
+            break;
+        case 13:
+            UIColor = "#d35400";
+            UILogo = "logo-13.jpg";
+            break;
+        case 0:
+            UIColor = "#c0392b";
+            UILogo = "logo-14.jpg";
+            break;
+    }
+
+    document.body.style.backgroundColor = UIColor;
+
+    document.getElementById("ui-logo").setAttribute("src", "assets/logos/" + UILogo + "")
+
+    document.getElementById("home-socialmedia1").style.color = UIColor;
+    document.getElementById("home-socialmedia2").style.color = UIColor;
+    document.getElementById("home-socialmedia3").style.color = UIColor;
+    document.getElementById("home-socialmedia4").style.color = UIColor;
+}
+
+
+function linkTo(page, where) {
+    if(where == "here") {
+        window.open(page, '_top');
+    }
+    else if(where == "newpage") {
+        window.open(page, '_blank');
+    }
+}
