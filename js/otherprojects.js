781bf3fb3a941e7b692c2508f3a145d755fc982e
LIVENVARANE <ulucqs@gmail.com>
2022-04-14 13:47:17 +0200

    Cleaned projects page code Redone mobile behavior on index.html

--- js/otherprojects.js
+++ js/otherprojects.js
@@ -1,55 +1,15 @@
-function otherProjectsNavbarSelect(id) { //Yes this is awful I did it two years ago and I'm not proud (and I probably never were)
-    if(document.getElementById(id) === null) throw 'NotFoundProjectItemError: The item specified was not found';
-
-    var value = document.getElementById(id).innerHTML;
-    if(value.includes("BasicDockHider")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("BasicDockHider").style.visibility = "visible";
-    } else if(value.includes("Emer")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("Emer").style.visibility = "visible";
-    } else if(value.includes("ETHSwitcher")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("ETHSwitcher").style.visibility = "visible";
-    } else if(value.includes("HapticSystem")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("HapticSystem").style.visibility = "visible";
-    } else if(value.includes("LIG")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("LIG").style.visibility = "visible";
-    } else if(value.includes("Music")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("Music").style.visibility = "visible";
-    } else if(value.includes("SlashSlash")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("SlashSlash").style.visibility = "visible";
-    } else if(value.includes("Sowy")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("Sowy").style.visibility = "visible";
-    } else if(value.includes("Project Ultima2")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("UltimaProject").style.visibility = "visible";
-    } else if(value.includes("Veddy")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("Veddy").style.visibility = "visible";
-    } else if(value.includes("WazeDecoy")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("WazeDecoy").style.visibility = "visible";
-    } else if(value.includes("ELB")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("ELB").style.visibility = "visible";
-    } else if(value.includes("EDBruteForce")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("EDBruteForce").style.visibility = "visible";
-    } else if(value.includes("RLTrader")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("RLTrader").style.visibility = "visible";
-    } else if(value.includes("DisquePlatine")) {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("DisquePlatine").style.visibility = "visible";
-    } else {
-        hideAllProjectNavbarDisplays();
-        document.getElementById("Error").style.visibility = "visible";
+function otherProjectsNavbarSelect(projectName) {
+    var error = true;
+    hideAllProjectNavbarDisplays();
+    document.querySelector('.navbar-display').querySelectorAll('div').forEach(function(item) {
+        if(projectName == item.getAttribute('name')) {
+            item.style.visibility = 'visible';
+            error = false;
+        }
+    });
+    if(error) {
+        document.getElementById('error').style.visibility = 'visible';
+        throw 'NotFoundProjectItemError: The item specified was not found';
     }
 }
 
@@ -65,4 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
     if(urlParams.has('goto')) {
         otherProjectsNavbarSelect(urlParams.get('goto'));
     }
+
+    var i = 0;
+    document.querySelector('.navbar').querySelectorAll('div').forEach(function(item) {
+        i++;
+        item.setAttribute('tabindex', i);
+    });
 });
